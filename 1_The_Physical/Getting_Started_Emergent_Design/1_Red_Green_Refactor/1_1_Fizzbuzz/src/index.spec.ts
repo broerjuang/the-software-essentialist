e3bc2d196500225d6a81918bb02aae16fba68f49
broerjuang <juangwiantoro@gmail.com>
2023-05-11 22:54:24 +0700

    [green] add another test case for different input

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -2,17 +2,18 @@ import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
   it('should return "Fizz" when input is divisible by 3', () => {
-    let result = fizzbuzz(3);
-    expect(result).toEqual("Fizz");
+    expect(fizzbuzz(3)).toEqual("Fizz");
+    expect(fizzbuzz(9)).toEqual("Fizz");
+    expect(fizzbuzz(42)).toEqual("Fizz");
   });
 
   it('should return "Buzz" when input is divisible by 5', () => {
-    let result = fizzbuzz(5);
-    expect(result).toEqual("Buzz");
+    expect(fizzbuzz(5)).toEqual("Buzz");
   });
 
   it('should return "FizzBuzz" when input is divisible by both 3 and 5', () => {
     let result = fizzbuzz(15);
     expect(result).toEqual("FizzBuzz");
+    expect(fizzbuzz(45)).toEqual("FizzBuzz");
   });
 });
