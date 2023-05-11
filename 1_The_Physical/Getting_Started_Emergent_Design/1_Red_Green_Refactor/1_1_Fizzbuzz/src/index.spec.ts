94eaff2f052a93a49e4e6dd50443a6c9bb56377f
broerjuang <juangwiantoro@gmail.com>
2023-05-11 22:36:12 +0700

    [red] add failing test for fizzbuzz to be divisible by 3

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it('should return "Fizz" when input is divisible by 3', () => {
+    let result = fizzbuzz(3);
+    expect(result).toEqual("Fizz");
+  });
 });
