e42ae49a34ccac4b9808e0eb076fb12bd7d3476c
broerjuang <juangwiantoro@gmail.com>
2023-05-11 22:42:29 +0700

    [red] add failing test for fizzbuzz divisible by 5

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,4 +5,9 @@ describe("fizzbuzz", () => {
     let result = fizzbuzz(3);
     expect(result).toEqual("Fizz");
   });
+
+  it('should return "Buzz" when input is divisible by 5', () => {
+    let result = fizzbuzz(5);
+    expect(result).toEqual("Buzz");
+  });
 });
