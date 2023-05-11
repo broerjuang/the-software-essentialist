99e5d510ab9de3c0319cefb882aea697773bd134
broerjuang <juangwiantoro@gmail.com>
2023-05-11 22:47:42 +0700

    [green] add implementation for FizzBuzz when the input is divisible by both 3 and 5

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,7 @@
 export function fizzbuzz(input: number) {
-  if (input % 3 === 0) {
+  if (input % 3 === 0 && input % 5 === 0) {
+    return "FizzBuzz";
+  } else if (input % 3 === 0) {
     return "Fizz";
   } else if (input % 5 === 0) {
     return "Buzz";
