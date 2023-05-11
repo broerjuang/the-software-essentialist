753a8b8692e61b7d1b2dfd07b80c56331249c849
broerjuang <juangwiantoro@gmail.com>
2023-05-11 23:20:06 +0700

    [red] add failing test for negative number

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -24,4 +24,8 @@ describe("fizzbuzz", () => {
   it('should throw "Number out of range" when input is greater than 100', () => {
     expect(() => fizzbuzz(102)).toThrow("Number out of range");
   });
+
+  it('should throw "Number out of range" when input is less than 0', () => {
+    expect(() => fizzbuzz(-2)).toThrow("Number out of range");
+  });
 });
