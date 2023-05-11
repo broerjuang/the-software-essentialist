692f35ea89a7741bd0ab95f084b2505013a0f620
broerjuang <juangwiantoro@gmail.com>
2023-05-11 23:21:33 +0700

    [green] add the implementation for negative number

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,7 +2,7 @@ export function fizzbuzz(input: number) {
   let isDivisibleBy3 = input % 3 === 0;
   let isDivisibleBy5 = input % 5 === 0;
 
-  if (input > 100) {
+  if (input > 100 || input < 0) {
     throw new Error("Number out of range");
   } else if (isDivisibleBy3 && isDivisibleBy5) {
     return "FizzBuzz";
