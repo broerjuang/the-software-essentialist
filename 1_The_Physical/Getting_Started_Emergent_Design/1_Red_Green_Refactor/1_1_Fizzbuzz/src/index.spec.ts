5ce5cc193654a7376d3b0759c35a166631953dc8
broerjuang <juangwiantoro@gmail.com>
2023-05-12 23:22:59 +0700

    [refactor] remove nested describe since the example or description inside it is clear enough to guess what the test is about

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,53 +1,40 @@
 import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-  describe('should return "Fizz" when input is divisible by 3', () => {
-    it.each([3, 9, 42])('should return "Fizz" when input is %s', (input) => {
+  it.each([3, 9, 42])('should return "Fizz" when input is %s', (input) => {
+    let result = fizzbuzz(input);
+    expect(result).toEqual("Fizz");
+  });
+
+  it.each([5, 10, 20, 25])('should return "Buzz" when input is %s', (input) => {
+    let result = fizzbuzz(input);
+    expect(result).toEqual("Buzz");
+  });
+
+  it.each([15, 30, 45])(
+    'should return "FizzBuzz" when input is %s',
+    (input) => {
       let result = fizzbuzz(input);
-      expect(result).toEqual("Fizz");
-    });
+      expect(result).toEqual("FizzBuzz");
+    }
+  );
+
+  it.each([
+    [4, "4"],
+    [7, "7"],
+    [8, "8"],
+  ])('should return "%s" when input is %s', (input, expected) => {
+    let result = fizzbuzz(input);
+    expect(result).toEqual(expected);
   });
 
-  describe('should return "Buzz" when input is divisible by 5', () => {
-    it.each([5, 10, 20, 25])(
-      'should return "Buzz" when input is %s',
-      (input) => {
-        let result = fizzbuzz(input);
-        expect(result).toEqual("Buzz");
-      }
-    );
-  });
-
-  describe('should return "FizzBuzz" when input is divisible by both 3 and 5', () => {
-    it.each([15, 30, 45])(
-      'should return "FizzBuzz" when input is %s',
-      (input) => {
-        let result = fizzbuzz(input);
-        expect(result).toEqual("FizzBuzz");
-      }
-    );
-  });
-
-  describe("should return the number as string when it is not divisible by 3 and 5", () => {
-    it.each([
-      [4, "4"],
-      [7, "7"],
-      [8, "8"],
-    ])('should return "%s" when input is %s', (input, expected) => {
-      let result = fizzbuzz(input);
-      expect(result).toEqual(expected);
-    });
-  });
-
-  describe('should throw "Number out of range" for input less than 0 or greater than 100', () => {
-    it.each([-2, -100, 101, 200])(
-      'should throw "Number out of range" when the input is %s',
-      (input) => {
-        expect(() => fizzbuzz(input)).toThrow("Number out of range");
-      }
-    );
-    it("should work with 0", () => {
-      expect(() => fizzbuzz(0)).not.toThrow();
-    });
+  it.each([-2, -100, 101, 200])(
+    'should throw "Number out of range" when the input is %s',
+    (input) => {
+      expect(() => fizzbuzz(input)).toThrow("Number out of range");
+    }
+  );
+  it("should work with 0", () => {
+    expect(() => fizzbuzz(0)).not.toThrow();
   });
 });
